6b0e7bda939e54c93a4a3355cfccaa641a8ca384
Svetlana Novikova <svetlana.novikova@jetbrains.com>
2023-04-15 23:13:01 +0200

    Change entrypoint.sh to pass multiline command

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,3 +13,18 @@ echo "Command 2: Execute idea.sh"
 echo "Command 3: Test existing artifact '$ARTIFACT'"
 test -e artifacts/$ARTIFACT
 EOF
+
+#!/bin/sh -l
+DOCKER_VERSION=$1
+
+echo "Pulling Docker image with version: $DOCKER_IMAGE_VERSION"
+docker pull your-docker-image:$DOCKER_VERSION
+
+echo "Running commands inside Docker container:"
+{
+  echo "set -e"
+  echo "echo 'Running: Execute idea.sh'"
+  echo "/opt/builder/bin/idea.sh helpbuilderinspect -source-dir . -product \$PRODUCT --runner github -output-dir artifacts/ || true"
+  echo "echo 'Running: Test existing artifact \$ARTIFACT'"
+  echo "test -e artifacts/\$ARTIFACT"
+} | docker run --rm -v ${PWD}:/workspace -e PRODUCT -e ARTIFACT registry.jetbrains.team/p/writerside/builder/writerside-builder:$DOCKER_VERSION /bin/bash -s
