0da3d96bb57e504c51d015387f48000dea763ce4
Svetlana Novikova <svetlana.novikova@jetbrains.com>
2023-06-11 23:01:34 +0200

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-- export DISPLAY=:99
-- Xvfb :99 &
+export DISPLAY=:99
+Xvfb :99 &
 /opt/builder/bin/idea.sh helpbuilderinspect -source-dir . -product $PRODUCT --runner github -output-dir artifacts/ || true
 echo "Test existing of $ARTIFACT artifact"
 test -e artifacts/$ARTIFACT
